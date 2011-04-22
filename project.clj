a08d6ccf29be49ce143871412ca8decf77c11542
fceccon <francesco.cek@me.com>
2011-04-22 13:54:17 +0200

    Manage the indexes

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "TODO: add summary of your project"
   :tasks [marginalia.tasks]
   :dependencies [[org.clojure/clojure "1.2.0"]
-		 [org.clojure/clojure-contrib "1.2.0"]]
+		 [org.clojure/clojure-contrib "1.2.0"]
+		 [clj-http "0.1.3"]]
   :dev-dependencies [[marginalia "0.5.0"]
 		     [midje "1.1-alpha-2"]])
--- src/indextank_clj/core.clj
+++ src/indextank_clj/core.clj
@@ -1,1 +1,45 @@
-(ns indextank-clj.core)
+(ns indextank-clj.core
+  (:require [clj-http.client :as http])
+  (:use [clojure.contrib.json :only [read-json]]))
+
+(def *private-url* nil)
+
+(defmacro with-client [client & body]
+  `(binding [*private-url* ~client]
+     (do ~@body)))
+
+(defn indexes
+  "Retrieves the metadata of every index in this account"
+  []
+  (let [resp (http/get (str *private-url* "/v1/indexes"))]
+    (if (= 200 (:status resp))
+      (read-json (:body resp))
+      nil)))
+
+(defn index-meta
+  "Retrieves metadata for the index name"
+  [name]
+  (let [resp (http/get (str *private-url* "/v1/indexes/" name))]
+    (if (= 200 (:status resp))
+      (read-json (:body resp))
+      nil)))
+
+(defn create-index
+  "Creates an index with the given name.
+It cannot contain forward slashes /"
+  [name]
+  (let [resp (http/put (str *private-url* "/v1/indexes/" name))
+	status (:status resp)]
+    (cond (= 200 status) (read-json (:body resp))
+	  (= 204 status) {:error " An index already existed for that name"}
+	  (= 409 status) {:error "Too many indexes for this account"})))
+
+;; IndexTank seems to not allow me to delete indexes, even from the web dashboard
+;; 
+(defn delete-index
+  "Removes the index name from the account"
+  [name]
+  (let [resp (http/delete (str *private-url* "/v1/indexes/" name))]
+    (if (= 200 (:status resp))
+      (read-json (:body resp))
+      nil)))
