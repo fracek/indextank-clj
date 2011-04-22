950e59d81b4675c99d718b5ef20b55b94820e099
fceccon <francesco.cek@me.com>
2011-04-22 14:39:18 +0200

    Cleanup code

--- src/indextank_clj/core.clj
+++ src/indextank_clj/core.clj
@@ -8,38 +8,35 @@
   `(binding [*private-url* ~client]
      (do ~@body)))
 
+(defmacro wrap-request [req-method req-url]
+  `(try
+     (let [resp# (http/request {:method ~req-method
+			       :url (str *private-url* ~req-url)})
+	   status# (:status resp#)]
+       (when (or (= 200 status#) (= 201 status#))
+	 (read-json (:body resp#))))
+     (catch Exception e#
+       (print e#))))
+
 (defn indexes
   "Retrieves the metadata of every index in this account"
   []
-  (let [resp (http/get (str *private-url* "/v1/indexes"))]
-    (if (= 200 (:status resp))
-      (read-json (:body resp))
-      nil)))
+  (wrap-request :get "/v1/indexes"))
 
 (defn index-meta
   "Retrieves metadata for the index name"
   [name]
-  (let [resp (http/get (str *private-url* "/v1/indexes/" name))]
-    (if (= 200 (:status resp))
-      (read-json (:body resp))
-      nil)))
+  (wrap-request :get (str "/v1/indexes/" name)))
 
 (defn create-index
   "Creates an index with the given name.
 It cannot contain forward slashes /"
   [name]
-  (let [resp (http/put (str *private-url* "/v1/indexes/" name))
-	status (:status resp)]
-    (cond (= 200 status) (read-json (:body resp))
-	  (= 204 status) {:error " An index already existed for that name"}
-	  (= 409 status) {:error "Too many indexes for this account"})))
+  (wrap-request :put (str "/v1/indexes/" name)))
 
 ;; IndexTank seems to not allow me to delete indexes, even from the web dashboard
-;; 
+;;
 (defn delete-index
   "Removes the index name from the account"
   [name]
-  (let [resp (http/delete (str *private-url* "/v1/indexes/" name))]
-    (if (= 200 (:status resp))
-      (read-json (:body resp))
-      nil)))
+  (wrap-request :delete (str "/v1/indexes/" name)))
