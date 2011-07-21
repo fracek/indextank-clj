f75d72de7edea88d49adb66b04663bc5d62c3f01
fceccon <francesco@ceccon.me>
2011-07-21 14:07:34 +0200

    Remove the ugly `defrequest` macro.

--- src/indextank_clj/core.clj
+++ src/indextank_clj/core.clj
@@ -10,7 +10,7 @@
      (do ~@body)))
 
 (defn- json-response
-  "Handle an empty response body"
+  "Handle an empty response body by returning true"
   [resp]
   (if (> (count (:body resp)) 0)
       (read-json (:body resp))
@@ -24,52 +24,39 @@
       (assoc base-req :body req-body)
       base-req)))
 
-(defmacro wrap-request
+(defn wrap-request
   "Make a request to req-url, if succeed returns the response body or true,
-if fail returns nil"
-  [req-method private-url req-url & [req-body]]
-  `(try (let [resp# (http/request (request-map ~req-method
-					      (str ~private-url ~req-url)
-					      ~req-body))
-	     status# (:status resp#)]
-	 (when (or (= 200 status#) (= 201 status#))
-	   (json-response resp#)))
-	(catch Exception e#
-	  (print e#))))
+   if fail returns nil"
+  [req-method req-url & [req-body]]
+  (try (let [resp (http/request (request-map req-method
+					      (str *private-url* req-url)
+					      req-body))
+	     status (:status resp)]
+	 (when (or (= 200 status) (= 201 status))
+	   (json-response resp)))
+	(catch Exception e
+	  (print e))))
 
-(defmacro defrequest
-  "Define a request, the last of args must be the private-url"
-  [name docstring args & body]
-  `(defn ~name
-     ~docstring
-      ([~@args]
-	 ~@body)
-      ([~@(butlast args)]
-	 (~name ~@(butlast args) *private-url*))))
-
-
-(defrequest indexes
+(defn indexes
   "Retrieves the metadata of every index in this account"
-  [private-url]
-  (wrap-request :get private-url "/v1/indexes"))
+  []
+  (wrap-request :get "/v1/indexes"))
 
-
-
-(defrequest index-meta
+(defn index-meta
   "Retrieves metadata for the index name"
-  [name private-url]
-  (wrap-request :get private-url (str "/v1/indexes/" name)))
+  [name]
+  (wrap-request :get (str "/v1/indexes/" name)))
 
-(defrequest create-index
+(defn create-index
   "Creates an index with the given name.
-It cannot contain forward slashes /"
-  [name private-url]
-  (wrap-request :put private-url (str "/v1/indexes/" name)))
+   It cannot contain forward slashes /"
+  [name]
+  (wrap-request :put (str "/v1/indexes/" name)))
 
-(defrequest delete-index
+(defn delete-index
   "Removes the index name from the account"
-  [name private-url]
-  (wrap-request :delete private-url (str "/v1/indexes/" name)))
+  [name]
+  (wrap-request :delete (str "/v1/indexes/" name)))
 
 (defn- valid-doc?
   "Check if the doc has :docid and :fields"
@@ -77,62 +64,62 @@ It cannot contain forward slashes /"
   (and (contains? doc :docid) (contains? doc :fields)))
 
 ;; TODO: merge add-document and add-documents
-(defrequest add-document
+(defn add-document
   "Adds a document to the index name"
-  [name doc private-url]
+  [name doc]
   (if (valid-doc? doc)
-    (wrap-request :put private-url
+    (wrap-request :put
 		  (str "/v1/indexes/" name "/docs") (json-str doc))
     (throw (Exception. "the doc map must have :docid and :fields"))))
 
-(defrequest add-documents
+(defn add-documents
   "Adds a batch of documents to the index name"
-  [name docs private-url]
+  [name docs]
   (if (every? valid-doc? docs)
-    (wrap-request :put private-url
+    (wrap-request :put
 		  (str "/v1/indexes/" name "/docs") (json-str docs))
     (throw (Exception. "the doc map must have :docid and :fields"))))
 
 ;; We use a querystring because we can't send a body in DELETE
-(defrequest delete-document
+(defn delete-document
   "Removes a document from the index name"
-  [name docid private-url]
-  (wrap-request :delete private-url
+  [name docid]
+  (wrap-request :delete
 		(str "/v1/indexes/" name "/docs?docid=" docid)))
 
-(defrequest score
+(defn score
   "Update the scoring variables of a document in index name"
-  [name docid score-map private-url]
+  [name docid score-map]
   (let [req-body {:docid docid :variables score-map}]
-    (wrap-request :put private-url
+    (wrap-request :put
 		  (str "/v1/indexes/" name "/docs/variables")
 		  (json-str req-body))))
 
-(defrequest categorize
+(defn categorize
   "Update the categories of a document in index name"
-  [name docid cat-map private-url]
+  [name docid cat-map]
   (let [req-body {:docid docid :categories cat-map}]
-    (wrap-request :put private-url
+    (wrap-request :put
 		  (str "/v1/indexes/" name "/docs/categories")
 		  (json-str req-body))))
 
-(defrequest functions
+(defn functions
   "Retrieves all the functions defined for the index name"
-  [name private-url]
-  (wrap-request :get private-url
+  [name]
+  (wrap-request :get
 		(str "/v1/indexes/" name "/functions")))
 
-(defrequest define-function
+(defn define-function
   "Defines the function number num for the index name"
-  [name fnum fdef private-url]
-  (wrap-request :put private-url
+  [name fnum fdef]
+  (wrap-request :put
 		(str "/v1/indexes/" name "/functions/" fnum)
 		(json-str {:definition fdef})))
 
-(defrequest delete-function
+(defn delete-function
   "Removes the function num from the index name"
-  [name fnum private-url]
-  (wrap-request :delete private-url
+  [name fnum]
+  (wrap-request :delete
 		(str "/v1/indexes/" name "/functions/" fnum)))
 
 
@@ -145,16 +132,18 @@ It cannot contain forward slashes /"
 	f (when fnum (str "&function=" fnum))]
     (str q s l f)))
 
-(defrequest search
+(defn search
   "Performs a search on the index name"
-  [name query private-url {start :start len :len fnum :function :as opts}]
-  (let [q-str (build-query query start len fnum)]
-    (wrap-request :get private-url
-		  (str "/v1/indexes/" name "/search?" q-str))))
+  ([name query]
+     (search name query {}))
+  ([name query {start :start len :len fnum :function :as opts}]
+      (let [q-str (build-query query start len fnum)]
+        (wrap-request :get
+                      (str "/v1/indexes/" name "/search?" q-str)))))
 
-(defrequest promote
+(defn promote
   "Promotes a document for a query on the index name"
-  [name docid query private-url]
-  (wrap-request :put private-url
+  [name docid query]
+  (wrap-request :put
 		(str "/v1/indexes/" name "/promote")
 		(json-str {:docid docid :query query})))
